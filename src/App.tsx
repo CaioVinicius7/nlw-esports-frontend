758ff260579c2f9ae1409c587f68a8e7469a8163
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-24 10:31:21 -0300

    refactor: :lipstick: Componentização do banner de criação de anúncio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { MagnifyingGlassPlus } from "phosphor-react";
-
 import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
 
 import "./styles/main.css";
 
@@ -36,23 +35,7 @@ export function App() {
 				<GameBanner bannerUrl="/game-6.png" title="Fortnite" adsCount={3} />
 			</div>
 
-			<div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-				<div className="bg-[#2a1634] px-8 py-6 flex justify-between items-center">
-					<div>
-						<strong className="text-white text-2xl font-black block">
-							Não encontrou seu duo?
-						</strong>
-						<span className="text-zinc-400 block">
-							Publique um anúncio para encontrar novos players!
-						</span>
-					</div>
-
-					<button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded transition-all flex items-center gap-3">
-						<MagnifyingGlassPlus size={24} />
-						Publicar anúncio
-					</button>
-				</div>
-			</div>
+			<CreateAdBanner />
 		</div>
 	);
 }
