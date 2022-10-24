88cc9d7ccfc7ad422aa71e8717a84294902c9921
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-24 12:09:41 -0300

    feat: :recycle: Consumo de API para listagem de games

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 
@@ -5,34 +7,47 @@ import "./styles/main.css";
 
 import logo from "./assets/logo.svg";
 
+interface Game {
+	id: string;
+	title: string;
+	banner: string;
+	_count: {
+		ads: number;
+	};
+}
+
 export function App() {
+	const [games, setGames] = useState<Game[]>([]);
+
+	async function fetchGames() {
+		fetch("http://localhost:3333/games")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setGames(data);
+			});
+	}
+
+	useEffect(() => {
+		fetchGames();
+	}, []);
+
 	return (
 		<div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
 			<img src={logo} alt="Logo" />
-
 			<h1 className="text-6xl text-white font-black mt-20">
 				Seu duo está aqui
 			</h1>
 
 			<div className="grid grid-cols-6 gap-6 mt-16">
-				<GameBanner
-					bannerUrl="/game-1.png"
-					title="League of Legends"
-					adsCount={3}
-				/>
-
-				<GameBanner bannerUrl="/game-2.png" title="Apex Legends" adsCount={3} />
-
-				<GameBanner bannerUrl="/game-3.png" title="CS: GO" adsCount={3} />
-
-				<GameBanner
-					bannerUrl="/game-4.png"
-					title="World of Warcraft"
-					adsCount={3}
-				/>
-				<GameBanner bannerUrl="/game-5.png" title="DOTA 2" adsCount={3} />
-
-				<GameBanner bannerUrl="/game-6.png" title="Fortnite" adsCount={3} />
+				{games.map((game) => (
+					<GameBanner
+						key={game.id}
+						bannerUrl={game.banner}
+						title={game.title}
+						adsCount={game._count.ads}
+					/>
+				))}
 			</div>
 
 			<CreateAdBanner />
