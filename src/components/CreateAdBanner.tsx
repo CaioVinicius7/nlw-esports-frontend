4c45ae12b28c97bcbacb776a483dc40c539c750c
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-26 11:05:27 -0300

    feat: :iphone: Responsividade do componente CreateAdBanner

--- src/components/CreateAdBanner.tsx
+++ src/components/CreateAdBanner.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassPlus } from "phosphor-react";
 export function CreateAdBanner() {
 	return (
 		<div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-			<div className="bg-[#2a1634] px-8 py-6 flex justify-between items-center">
+			<div className="bg-[#2a1634] px-8 py-6 flex flex-col justify-between gap-8 md:flex-row md:items-center">
 				<div>
 					<strong className="text-white text-2xl font-black block">
 						Não encontrou seu duo?
@@ -14,7 +14,7 @@ export function CreateAdBanner() {
 					</span>
 				</div>
 
-				<Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded transition-all flex items-center gap-3">
+				<Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded transition-all flex items-center justify-center gap-3 w-full md:w-auto">
 					<MagnifyingGlassPlus size={24} />
 					Publicar anúncio
 				</Dialog.Trigger>
